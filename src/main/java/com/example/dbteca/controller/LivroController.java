6cabd85224f8f7247b0e154e036914a1845c0075
Alisson Pazze <alisson_pazze@terceiros.sicredi.com.br>
2022-06-20 09:09:49 -0300

    Adicionado validação no Nome do Autor para ser unico no cadastro do Livro. Duplicação de código, será refatorado e centralizado em Services.

--- src/main/java/com/example/dbteca/controller/LivroController.java
+++ src/main/java/com/example/dbteca/controller/LivroController.java
@@ -1,6 +1,7 @@
 package com.example.dbteca.controller;
 
 import com.example.dbteca.entity.Livro;
+import com.example.dbteca.repository.AutorRepository;
 import com.example.dbteca.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +11,14 @@ import org.springframework.web.bind.annotation.*;
 public class LivroController {
     @Autowired
     private LivroRepository livroRepository;
+    @Autowired
+    private AutorRepository autorRepository;
 
     @PostMapping(path = "/inserir")
     public Livro Adicionar(@RequestBody Livro livro) {
+        if(autorRepository.findByNome(livro.getAutor().getNome()).isPresent()){
+            throw new IllegalArgumentException("Nome ja existe no banco. Tente outro!");
+        }
         return livroRepository.save(livro);
     }
     @GetMapping
