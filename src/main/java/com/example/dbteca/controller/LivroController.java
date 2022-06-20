a97828b15b0fe5ad22fb94a69754abb9a7576902
Alisson Pazze <alisson_pazze@terceiros.sicredi.com.br>
2022-06-20 08:15:25 -0300

    Adicionado busca do Livro por Isbn.

--- src/main/java/com/example/dbteca/controller/LivroController.java
+++ src/main/java/com/example/dbteca/controller/LivroController.java
@@ -3,10 +3,7 @@ package com.example.dbteca.controller;
 import com.example.dbteca.entity.Livro;
 import com.example.dbteca.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/livro")
@@ -17,5 +14,9 @@ public class LivroController {
     @PostMapping(path = "/inserir")
     public Livro Adicionar(@RequestBody Livro livro) {
         return livroRepository.save(livro);
+    }
+    @GetMapping
+    public Livro BuscaPorIsbn(@RequestParam String isbn){
+        return livroRepository.findByIsbn(isbn);
     }
 }
--- src/main/java/com/example/dbteca/repository/LivroRepository.java
+++ src/main/java/com/example/dbteca/repository/LivroRepository.java
@@ -4,4 +4,5 @@ import com.example.dbteca.entity.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+    Livro findByIsbn(String isbn);
 }
