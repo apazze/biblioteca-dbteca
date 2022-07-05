ca1a930eaf1abb73ee037f5ed22ed70724852089
Alisson Pazze <alisson_pazze@terceiros.sicredi.com.br>
2022-07-05 19:59:58 -0300

    Criado teste para nome do Autor repetido.

--- src/test/java/com/example/dbteca/service/AutorServiceTest.java
+++ src/test/java/com/example/dbteca/service/AutorServiceTest.java
@@ -5,20 +5,26 @@ import com.example.dbteca.mapper.AutorMapper;
 import com.example.dbteca.repository.AutorRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@DataJpaTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
 class AutorServiceTest {
+
+    @Autowired
+    private AutorRepository autorRepository;
     @Autowired
     private AutorService autorService;
-    @MockBean
-    private AutorRepository autorRepository;
 
     @Test
     void NaoDeveCadastrarAutorExistente() {
         Autor autor = new Autor("autor");
         autorService.Adicionar(AutorMapper.ParaAutorDto(autor));
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> autorService.Adicionar(AutorMapper.ParaAutorDto(autor)));
+        assertEquals("Nome do Autor ja existe!!!!!!!!", exception.getMessage());
     }
 
 }
